178715dd43d7cdb9dd5b385aae59d81dcc273725
arfan36 <arfanur36@gmail.com>
2022-11-20 13:03:53 +0600

    71-4 blur background image added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -3,8 +3,8 @@ import chair from '../../../assets/images/chair.png';
 
 const Banner = () => {
     return (
-        <div className="hero">
-            <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero rounded-xl" style={{ backgroundImage: `url(${chair})` }}>
+            <div className="hero-content flex-col lg:flex-row-reverse backdrop-blur-sm bg-white/50 rounded-xl">
                 <img src={chair} className="w-1/2 rounded-lg shadow-2xl" alt='' />
                 <div>
                     <h1 className="text-5xl font-bold">Box Office News!</h1>
