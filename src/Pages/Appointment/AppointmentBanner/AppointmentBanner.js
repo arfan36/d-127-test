a66e4d4a62d0650d9516c615f3b32460023a2d9b
arfan36 <arfanur36@gmail.com>
2022-11-22 19:11:50 +0600

    72-3 Lift up state to share data and Available Appointment

--- src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner/AppointmentBanner.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import chair from '../../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
-import { format } from 'date-fns';
 
-const AppointmentBanner = () => {
-    const [selectedDate, setSelectedDate] = useState(new Date());
+const AppointmentBanner = ({ selectedDate, setSelectedDate }) => {
     return (
-        <header className='my-6'>
-            <div className="hero">
+        <header className='my-6 rounded-xl'
+            style={{ background: `url(${chair})` }}
+        >
+            <div className="hero backdrop-blur-sm bg-white/50">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt='dentist chair' />
                     <div className='mr-6'>
@@ -16,7 +16,6 @@ const AppointmentBanner = () => {
                             selected={selectedDate}
                             onSelect={setSelectedDate}
                         />
-                        <p>You have selected date: {format(selectedDate, 'PP')}</p>
                     </div>
                 </div>
             </div>
--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,16 +4,18 @@ import PrimaryButton from '../../../components/PrimaryButton/PrimaryButton';
 
 const Banner = () => {
     return (
-        <div className="hero rounded-xl" style={{ backgroundImage: `url(${chair})` }}>
-            <div className="hero-content flex-col lg:flex-row-reverse backdrop-blur-sm bg-white/50 rounded-xl">
-                <img src={chair} className="lg:w-1/2 rounded-lg shadow-2xl" alt='' />
-                <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <PrimaryButton>Get Started</PrimaryButton>
+        <section style={{ background: `url(${chair})` }} className="rounded-xl">
+            <div className="hero backdrop-blur-sm bg-white/50 rounded-xl" >
+                <div className="hero-content flex-col lg:flex-row-reverse ">
+                    <img src={chair} className="lg:w-1/2 rounded-lg shadow-2xl" alt='' />
+                    <div>
+                        <h1 className="text-5xl font-bold">Box Office News!</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <PrimaryButton>Get Started</PrimaryButton>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
 
