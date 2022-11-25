67b71bb693d08fc463aab249466a544dae81ca2a
arfan36 <arfanur36@gmail.com>
2022-11-25 15:18:07 +0600

    73-7 (Recap) Implement Login and auth state observer

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const Login = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const { signIn } = useContext(AuthContext);
+    const [loginError, setLoginError] = useState('');
 
     const handleLogin = (data) => {
         console.log(data);
+        const { email, password } = data;
+        setLoginError('');
+        signIn(email, password).then((result) => {
+            const user = result.user;
+            console.log("🚀 ~ user", user);
+        }).catch((err) => {
+            console.error('err', err.message);
+            setLoginError(err.message);
+        });
     };
 
     return (
@@ -42,6 +54,9 @@ const Login = () => {
                         </label>
                     </div>
                     <input className='btn btn-accent w-full' value={'Login'} type="submit" />
+                    <div>
+                        {loginError && <p className='text-error'>{loginError}</p>}
+                    </div>
                 </form>
                 <p>New to Doctors Portal <Link className='text-secondary' to={'/signup'}>Create New Account</Link></p>
                 <div className="divider">OR</div>
--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,18 +1,43 @@
-import React, { createContext } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
     // create user with email and password
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
+    // sign in with email and password
+    const signIn = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    // observer
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('user observing');
+            setUser(currentUser);
+            if (currentUser === null || currentUser.emailVerified) {
+            }
+            setLoading(false);
+        });
+        return () => {
+            unsubscribe();
+        };
+    }, []);
+
+
     const authInfo = {
-        createUser
+        createUser,
+        signIn,
+        user
     };
     return (
         <AuthContext.Provider value={authInfo}>
