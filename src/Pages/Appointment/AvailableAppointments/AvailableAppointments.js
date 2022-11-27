1e61266da2261021b87395ca2e0aa24ab3002a95
arfan36 <arfanur36@gmail.com>
2022-11-27 13:19:54 +0600

    74-3 Simple Overview of React Query and load data

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,3 +1,4 @@
+import { async } from '@firebase/util';
 import { useQuery } from '@tanstack/react-query';
 import { format } from 'date-fns';
 import React, { useState } from 'react';
@@ -5,20 +6,16 @@ import BookingModal from '../BookingModal/BookingModal';
 import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointments = ({ selectedDate }) => {
-    // const [appointmentOptions, setAppointmentOptions] = useState([]);
     const [treatment, setTreatment] = useState(null);
 
     const { data: appointmentOptions = [] } = useQuery({
         queryKey: ['appointmentOptions'],
-        queryFn: () => fetch(`http://localhost:5000/appointmentOptions`)
-            .then(res => res.json())
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/appointmentOptions`);
+            const data = await res.json();
+            return data;
+        }
     });
-
-    // useEffect(() => {
-    //     fetch(`http://localhost:5000/appointmentOptions`)
-    //         .then(res => res.json())
-    //         .then(data => setAppointmentOptions(data));
-    // }, []);
 
     return (
         <section className='my-16'>
