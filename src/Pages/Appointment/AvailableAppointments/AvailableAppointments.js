8eb0b6336c7dc56859a4aefd674ca507927f383d
arfan36 <arfanur36@gmail.com>
2022-11-27 19:11:51 +0600

    74-5 (Interesting) Find available time slots for a day

--- src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/Pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -6,11 +6,11 @@ import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointments = ({ selectedDate }) => {
     const [treatment, setTreatment] = useState(null);
-
+    const date = format(selectedDate, 'PP');
     const { data: appointmentOptions = [] } = useQuery({
-        queryKey: ['appointmentOptions'],
+        queryKey: ['appointmentOptions', date],
         queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/appointmentOptions`);
+            const res = await fetch(`http://localhost:5000/appointmentOptions?date=${date}`);
             const data = await res.json();
             return data;
         }
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -5,7 +5,7 @@ import { AuthContext } from '../../../contexts/AuthProvider';
 
 const BookingModal = ({ treatment, setTreatment, selectedDate }) => {
     // treatment is just another name of appointmentOption with name, slots, _id
-    const { name, slots } = treatment;
+    const { name: treatmentName, slots } = treatment;
     const date = format(selectedDate, 'PP');
     const { user } = useContext(AuthContext);
 
@@ -19,7 +19,7 @@ const BookingModal = ({ treatment, setTreatment, selectedDate }) => {
 
         const booking = {
             appointmentDate: date,
-            treatment: name,
+            treatment: treatmentName,
             patient: patientName,
             slot,
             email,
@@ -54,7 +54,7 @@ const BookingModal = ({ treatment, setTreatment, selectedDate }) => {
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">{name}</h3>
+                    <h3 className="text-lg font-bold">{treatmentName}</h3>
 
                     <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
                         <input type="text" value={date} disabled className="input input-bordered w-full" />
