677c12a67618324069d9a5a39ae4e0736b50c9f8
arfan36 <arfanur36@gmail.com>
2022-11-28 10:14:49 +0600

    74-8

--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -44,6 +44,9 @@ const BookingModal = ({ treatment, setTreatment, selectedDate, refetch }) => {
                     toast.success('Booking Confirmed');
                     refetch();
                 }
+                else {
+                    toast.error(data.message);
+                }
             })
             .catch(err => console.error('err', err));
 
