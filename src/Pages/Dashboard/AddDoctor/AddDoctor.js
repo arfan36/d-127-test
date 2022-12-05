26a396596bcf2a79218b75c1b367eb898c5c2922
arfan36 <arfanur36@gmail.com>
2022-12-05 13:45:21 +0600

    76-2 Load services with data project and Add file type input

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const AddDoctor = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleAddDoctor = (data) => {
+        console.log("🚀 ~ data", data);
+
+    };
     return (
-        <div>
+        <div className='w-96 p-7'>
             <h2 className="text-4xl">Add a Doctor</h2>
+            <form onSubmit={handleSubmit(handleAddDoctor)} >
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Name</span>
+                    </label>
+                    <input type="text" {...register("name", {
+                        required: "Name is required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.name && <p className='text-error'>{errors.name.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="email" {...register("email", {
+                        required: "Email is required"
+                    })} className="input input-bordered w-full max-w-xs" />
+                    {errors.email && <p className='text-error'>{errors.email.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Specialty</span>
+                    </label>
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option>Pick a Specialty</option>
+                        <option>Svelte</option>
+                        <option>Vue</option>
+                        <option>React</option>
+                    </select>
+                </div>
+                <input className='btn btn-accent w-full mt-3' value={'Add Doctor'} type="submit" />
+                {/* {
+                    signUpError && <p className='text-error'>{signUpError}</p>
+                } */}
+            </form>
         </div>
     );
 };
