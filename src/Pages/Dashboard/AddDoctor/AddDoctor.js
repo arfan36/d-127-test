6b44e2dec77752aa00af34f3ed57bcff6be8acde
arfan36 <arfanur36@gmail.com>
2022-12-06 18:43:50 +0600

    76-3 File Type image field and understand form data

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,12 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import Loading from '../../Shared/Loading/Loading';
 
 const AddDoctor = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const { data: specialties, isLoading } = useQuery({
+        queryKey: ['specialty'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/appointmentSpecialty`);
+            const data = await res.json();
+            return data;
+        }
+    });
+
     const handleAddDoctor = (data) => {
         console.log("🚀 ~ data", data);
 
     };
+
+    if (isLoading) {
+        return <Loading></Loading>;
+    }
+
     return (
         <div className='w-96 p-7'>
             <h2 className="text-4xl">Add a Doctor</h2>
@@ -33,12 +50,31 @@ const AddDoctor = () => {
                     <label className="label">
                         <span className="label-text">Specialty</span>
                     </label>
-                    <select className="select select-ghost w-full max-w-xs">
-                        <option>Pick a Specialty</option>
-                        <option>Svelte</option>
-                        <option>Vue</option>
-                        <option>React</option>
+                    <select
+                        {
+                        ...register("specialty", {
+                            required: "Select any option"
+                        })
+                        }
+                        className="select input-bordered w-full max-w-xs">
+                        {
+                            specialties &&
+                            specialties.map(specialty => <option
+                                key={specialty._id}
+                                value={specialty.name}
+                            >{specialty.name}</option>)
+                        }
                     </select>
+                    {errors.specialty && <p className='text-error'>{errors.specialty.message}</p>}
+                </div>
+                <div className="form-control w-full max-w-xs">
+                    <label className="label">
+                        <span className="label-text">Photo</span>
+                    </label>
+                    <input type="file" {...register("img", {
+                        required: "Photo is required"
+                    })} className="input w-full max-w-xs" />
+                    {errors.img && <p className='text-error'>{errors.img.message}</p>}
                 </div>
                 <input className='btn btn-accent w-full mt-3' value={'Add Doctor'} type="submit" />
                 {/* {
