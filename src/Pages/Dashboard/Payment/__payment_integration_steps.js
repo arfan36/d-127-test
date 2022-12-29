b0b98335de0479c89da4fb3b157268f4383c0662
arfan36 <arfanur36@gmail.com>
2022-12-29 12:16:30 +0600

    77-6 Server side payment intents API to get client Secret from stripe

---
+++ src/Pages/Dashboard/Payment/__payment_integration_steps.js
@@ -0,0 +1,10 @@
+/* 
+// ─── Collect Card Information ────────────────────────────────────────────────
+* 1. stripe install
+* 2. card loadStripe
+* : pk (Publishable key)
+* 3. card element
+* 4. card form
+* 5. stripe, elements
+* 6. check card error and display error
+ */
